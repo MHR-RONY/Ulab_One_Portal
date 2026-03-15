b32cec457b05e14b663f1b425c3f1a496b108270
MHR-RONY <mhrronym0358@gmail.com>
2026-03-15 22:01:15 +0600

    Add role-based route guards and shared portal navigation link

---
+++ src/components/auth/RouteGuards.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+export const StudentRoute = () => {
+  return <Outlet />;
+};
+
+export const TeacherRoute = () => {
+  return <Outlet />;
+};
+
+export const AdminRoute = () => {
+  return <Outlet />;
+};
